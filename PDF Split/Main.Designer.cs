80063e9895b16e9a9e33e9d20b059d9c6179a250
Marcus Schuff <marcus.schuff@gmail.com>
2019-08-02 16:58:55 -0500

    boomshakabowow

--- PDF Split/Main.Designer.cs
+++ PDF Split/Main.Designer.cs
@@ -29,115 +29,146 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtInstructions = new System.Windows.Forms.RichTextBox();
+            this.lblPageCount = new System.Windows.Forms.Label();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.txtPages = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnBrowse = new System.Windows.Forms.Button();
             this.txtFile = new System.Windows.Forms.TextBox();
             this.lblFile = new System.Windows.Forms.Label();
-            this.txtPages = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnSplit = new System.Windows.Forms.Button();
-            this.lblPageCount = new System.Windows.Forms.Label();
-            this.txtInstructions = new System.Windows.Forms.RichTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label3
+            // splitContainer1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 28);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(277, 17);
-            this.label3.TabIndex = 21;
-            this.label3.Text = "Select or drag and drop a PDF file to split.";
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
             // 
-            // btnBrowse
+            // splitContainer1.Panel1
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(515, 63);
-            this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(113, 31);
-            this.btnBrowse.TabIndex = 20;
-            this.btnBrowse.Text = "Browse";
-            this.btnBrowse.UseVisualStyleBackColor = true;
-            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            this.splitContainer1.Panel1.Controls.Add(this.txtInstructions);
+            this.splitContainer1.Panel1.Controls.Add(this.lblPageCount);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSplit);
+            this.splitContainer1.Panel1.Controls.Add(this.txtPages);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.btnBrowse);
+            this.splitContainer1.Panel1.Controls.Add(this.txtFile);
+            this.splitContainer1.Panel1.Controls.Add(this.lblFile);
             // 
-            // txtFile
+            // splitContainer1.Panel2
             // 
-            this.txtFile.Location = new System.Drawing.Point(133, 67);
-            this.txtFile.Name = "txtFile";
-            this.txtFile.Size = new System.Drawing.Size(376, 23);
-            this.txtFile.TabIndex = 19;
-            // 
-            // lblFile
-            // 
-            this.lblFile.AutoSize = true;
-            this.lblFile.Location = new System.Drawing.Point(33, 70);
-            this.lblFile.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.lblFile.Name = "lblFile";
-            this.lblFile.Size = new System.Drawing.Size(91, 17);
-            this.lblFile.TabIndex = 18;
-            this.lblFile.Text = "Selected File:";
-            // 
-            // txtPages
-            // 
-            this.txtPages.Location = new System.Drawing.Point(133, 133);
-            this.txtPages.Name = "txtPages";
-            this.txtPages.Size = new System.Drawing.Size(376, 23);
-            this.txtPages.TabIndex = 24;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 136);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 17);
-            this.label1.TabIndex = 23;
-            this.label1.Text = "Page Ranges:";
-            // 
-            // btnSplit
-            // 
-            this.btnSplit.Location = new System.Drawing.Point(515, 129);
-            this.btnSplit.Name = "btnSplit";
-            this.btnSplit.Size = new System.Drawing.Size(113, 31);
-            this.btnSplit.TabIndex = 25;
-            this.btnSplit.Text = "Split";
-            this.btnSplit.UseVisualStyleBackColor = true;
-            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
-            // 
-            // lblPageCount
-            // 
-            this.lblPageCount.AutoSize = true;
-            this.lblPageCount.Location = new System.Drawing.Point(130, 93);
-            this.lblPageCount.Name = "lblPageCount";
-            this.lblPageCount.Size = new System.Drawing.Size(99, 17);
-            this.lblPageCount.TabIndex = 26;
-            this.lblPageCount.Text = "Page Count:  ";
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(1204, 787);
+            this.splitContainer1.SplitterDistance = 507;
+            this.splitContainer1.TabIndex = 28;
             // 
             // txtInstructions
             // 
             this.txtInstructions.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtInstructions.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.txtInstructions.Enabled = false;
-            this.txtInstructions.Location = new System.Drawing.Point(133, 179);
+            this.txtInstructions.Location = new System.Drawing.Point(36, 175);
             this.txtInstructions.Name = "txtInstructions";
-            this.txtInstructions.Size = new System.Drawing.Size(495, 135);
-            this.txtInstructions.TabIndex = 27;
+            this.txtInstructions.Size = new System.Drawing.Size(424, 568);
+            this.txtInstructions.TabIndex = 36;
             this.txtInstructions.Text = "";
+            // 
+            // lblPageCount
+            // 
+            this.lblPageCount.AutoSize = true;
+            this.lblPageCount.Location = new System.Drawing.Point(136, 74);
+            this.lblPageCount.Name = "lblPageCount";
+            this.lblPageCount.Size = new System.Drawing.Size(99, 17);
+            this.lblPageCount.TabIndex = 35;
+            this.lblPageCount.Text = "Page Count:  ";
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(347, 110);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(113, 31);
+            this.btnSplit.TabIndex = 34;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            // 
+            // txtPages
+            // 
+            this.txtPages.Location = new System.Drawing.Point(139, 114);
+            this.txtPages.Name = "txtPages";
+            this.txtPages.Size = new System.Drawing.Size(202, 23);
+            this.txtPages.TabIndex = 33;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Page Ranges:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 17);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Select or drag and drop a PDF file to split.";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(347, 44);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(113, 31);
+            this.btnBrowse.TabIndex = 30;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(139, 48);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(202, 23);
+            this.txtFile.TabIndex = 29;
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(39, 51);
+            this.lblFile.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(91, 17);
+            this.lblFile.TabIndex = 28;
+            this.lblFile.Text = "Selected File:";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(693, 787);
+            this.webBrowser1.TabIndex = 0;
             // 
             // Main
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 326);
-            this.Controls.Add(this.txtInstructions);
-            this.Controls.Add(this.lblPageCount);
-            this.Controls.Add(this.btnSplit);
-            this.Controls.Add(this.txtPages);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.txtFile);
-            this.Controls.Add(this.lblFile);
+            this.ClientSize = new System.Drawing.Size(1204, 787);
+            this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -146,22 +177,28 @@
             this.Load += new System.EventHandler(this.Main_Load);
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Main_DragEnter);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox txtInstructions;
+        private System.Windows.Forms.Label lblPageCount;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.TextBox txtPages;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.TextBox txtFile;
         private System.Windows.Forms.Label lblFile;
-        private System.Windows.Forms.TextBox txtPages;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnSplit;
-        private System.Windows.Forms.Label lblPageCount;
-        private System.Windows.Forms.RichTextBox txtInstructions;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
