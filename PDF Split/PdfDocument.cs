641344ed2b545cf16160f4a93d3785c674f15996
hambone-the-Great <marcus.schuff@gmail.com>
2022-02-18 17:39:33 -0800

    gotta figure out why this isn't working correctly.
    where do I want to go to get a beer tonight?

---
+++ PDF Split/PdfDocument.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_Split.Tools
+{
+    public class PdfDocument
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<int> Pages { get; set; }
+
+
+        public PdfDocument()
+        {
+
+        }
+
+
+
+
+    }
+}
